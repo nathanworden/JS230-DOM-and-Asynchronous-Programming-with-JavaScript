d6cac97fe50e2cc93c973a276e3ced651cf28d05
Nathan Worden <worden.nathan@gmail.com>
2020-06-15 10:53:19 -0700

    Making HTTP Requests from JavaScript

---
+++ 03.Making_HTTP_Requests_from_JavaScript/02.what_to_focus_on.js
@@ -0,0 +1,22 @@
+// What to Focus On
+
+// Think HTTP
+// HTTP is the protocol used by the network operations we cover in this lesson. You must be familiar with the request-response cycle, the components of each part and how you can access these values from JavaScript.
+
+// Be Familiar with XMLHttpRequest
+// You should know how to use `XMLHttpRequest` to send requests and how to execute code at specific points as the request moves through its lifecycle.
+
+// You also need to know how to handle successful and unsuccessful requests.
+
+// Understand How to Serialize Data
+
+// You should know how to use `XMLHttpRequest` to submit a form from a webpage and send requests using JSON formatted data.
+
+// Decompose Functionality into API Requests
+
+// While each API is different, you should be able to translate a requirement that says "create a post on the server" into "make a POST request to `/posts` with the required JSON data" and be able to write the code that does it.
+
+// Read API Documentations
+// Don't try to memorize the details of the web APIs we discuss. Instead, try to internalize the conventions you see and learn to find the specifics in the documentation.
+
+// On some projects, the documentation will be in your teammates' minds. In such cases, you must be able to talk to them about the URL, parameters, etc. needed for a piece of functionality.
