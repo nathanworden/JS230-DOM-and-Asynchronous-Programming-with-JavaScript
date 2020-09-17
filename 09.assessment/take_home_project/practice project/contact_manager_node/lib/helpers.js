1eff07ac0579288a07967a695798cfbcb27d9d28
Nathan Worden <worden.nathan@gmail.com>
2020-09-16 17:05:56 -0700

    update helpers

--- 09.assessment/take_home_project/practice project/contact_manager_node/lib/helpers.js
+++ 09.assessment/take_home_project/practice project/contact_manager_node/lib/helpers.js
@@ -1,4 +1,4 @@
-const CONTACT_ATTRS = ['full_name', 'phone_number', 'email', 'tags'];
+const CONTACT_ATTRS = ['full_name', 'phone_number', 'email', 'tags', 'sneaky place to hide something'];
 
 module.exports = {
   extractContactAttrs: function(body) {
